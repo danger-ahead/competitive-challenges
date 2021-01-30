da8a9b5e26c0a95f805a2dac2c1b6203ecbf823c
Shourya Shikhar Ghosh <shourya.personal.999@gmail.com>
2021-01-30 14:45:11 +0530

    Initial Commit

---
+++ Hackerrank/Day_4_Class_vs_Instance.py
@@ -0,0 +1,66 @@
+"""Task
+Write a Person class with an instance variable, age, and a constructor that takes an integer, initialAge,
+as a parameter. The constructor must assign initialAge to age after confirming the argument passed as initialAge
+is not negative; if a negative argument is passed as initialAge, the constructor should set age to 0
+and print Age is not valid, setting age to 0.. In addition, you must write the following instance methods:
+1. yearPasses() should increase the age instance variable by 1.
+2. amIOld() should perform the following conditional actions:
+2.1 If  age < 13 , print You are young..
+2.2 If age >= 13 and age < 18, print You are a teenager..
+2.3 Otherwise, print You are old..
+Input Format
+Input is handled for you by the stub code in the editor.
+The first line contains an integer, T (the number of test cases),
+and the subsequent lines each contain an integer denoting the age of a Person instance."""
+
+
+class Person:
+    def __init__(self, initialAge):
+        # Add some more code to run some checks on initialAge
+        if not initialAge < 0:
+            self.age = initialAge
+        else:
+            self.age = 0
+            print("Age is not valid, setting age to 0.")
+
+    def amIOld(self):
+        # Do some computations in here and print out the correct statement to the console
+        if self.age < 13:
+            print("You are young.")
+        elif 13 <= self.age < 18:
+            print("You are a teenager.")
+        else:
+            print("You are old.")
+
+    def yearPasses(self):
+        self.age = self.age + 1
+
+
+# Increment the age of the person in here
+
+t = int(input())
+for i in range(0, t):
+    age = int(input())
+    p = Person(age)
+    p.amIOld()
+    for j in range(0, 3):
+        p.yearPasses()
+    p.amIOld()
+    print("")
+
+"""Task
+Write a Person class with an instance variable, age, and a constructor that takes an integer, initialAge, 
+as a parameter. The constructor must assign initialAge to age after confirming the argument passed as initialAge 
+is not negative; if a negative argument is passed as initialAge, the constructor should set age to 0
+and print Age is not valid, setting age to 0.. In addition, you must write the following instance methods:
+1. yearPasses() should increase the age instance variable by 1.
+2. amIOld() should perform the following conditional actions:
+2.1 If  age < 13 , print You are young..
+2.2 If age >= 13 and age < 18, print You are a teenager.. 
+2.3 Otherwise, print You are old..
+Input Format
+Input is handled for you by the stub code in the editor.
+The first line contains an integer, T (the number of test cases), 
+and the subsequent lines each contain an integer denoting the age of a Person instance."""
+
+
